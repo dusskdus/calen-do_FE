8507f934ebf7c66d66a8323c67266dfa20818457
idid10 <ejwuud2@naver.com>
2025-02-16 23:44:55 +0900

    fix: 첫페이지 로그인

--- calendo-fe/src/Routes.js
+++ calendo-fe/src/Routes.js
@@ -12,7 +12,7 @@ const Router = () => {
         <BrowserRouter>
             <div>
                 <Routes>
-                    <Route path="/" element={<WholeSchedule />} /> {/* 기본 경로는 로그인 페이지 */}
+                    <Route path="/" element={<Login />} /> {/* 기본 경로는 로그인 페이지 */}
                     <Route path="/whole-schedule" element={<WholeSchedule />} /> {/* 로그인 성공 시 이동할 페이지 */}
                     <Route path="/home" element={<Home />} /> {/* 기존 Home 페이지는 /home 경로로 이동 */}
                     <Route path="/invite" element={<InvitePage />} />
