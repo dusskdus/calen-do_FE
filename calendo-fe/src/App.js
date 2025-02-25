390f765847e133e23348f879822fd9ce04298eea
idid10 <ejwuud2@naver.com>
2025-02-25 16:41:19 +0900

    api:로그인 프백연동

--- calendo-fe/src/App.js
+++ calendo-fe/src/App.js
@@ -1,11 +1,23 @@
-import Router from "./Routes";
+import AppRouter from "./Routes";
 import { GlobalStyle } from "./styles/globalStyles";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/login/Login";
+import WholeSchedule from "./pages/schedule/WholeSchedule";
+import OAuthRedirectHandler from "./pages/login/OAuthRedirectHandler"; // ✅ 추가
+
 
 function App() {
   return (
     <>
     <GlobalStyle />
-    <Router />
+    <Router>
+      {/* 🔥 `Routes` 태그 안에 `Route`를 감싸야 함 */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login/oauth2/code/google" element={<OAuthRedirectHandler />} />
+        <Route path="/whole-schedule" element={<WholeSchedule />} />
+      </Routes>
+    </Router>
     </>
   );
 }
